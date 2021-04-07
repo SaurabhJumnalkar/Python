9fe29fe8188f7f60e53106200fedf530c4f9a324
Saurabh sudhir jumnalkar <saurabhjum@gmail.com>
2021-04-07 15:21:25 +0530

    Date and Time Handling in python Folder added

---
+++ Date ANd Time/date.py
@@ -0,0 +1,6 @@
+import datetime
+
+x=datetime.datetime.now()
+print(x)
+print(x.month)
+print(x.year)
---
+++ Date ANd Time/dateOutput.py
@@ -0,0 +1,6 @@
+import datetime
+
+x=datetime.datetime(2080,6,30)
+print(x)
+print(x.month)
+print(x.year)
